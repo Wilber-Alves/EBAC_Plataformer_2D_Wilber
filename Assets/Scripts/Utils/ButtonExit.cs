943d01ad4e1216721dcfb43ae6e2e2ac9c6f8402
Wilber <wilber.sa@hotmail.com>
2025-11-04 21:22:27 -0300

    Exit button update
    It is now possible to click the exit button to quit and turn off the game directly in UNITY, in addition to the debug message from the Quit() function.

--- Assets/Scripts/Utils/ButtonExit.cs
+++ Assets/Scripts/Utils/ButtonExit.cs
@@ -5,6 +5,11 @@ public class ButtonExit : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+        Debug.Log("Game is exiting...");
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 
 }
