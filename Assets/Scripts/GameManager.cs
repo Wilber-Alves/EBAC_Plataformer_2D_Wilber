d25fadcba05d5770c72166e39ce33dd7449dea5f
Wilber <wilber.sa@hotmail.com>
2025-11-04 21:25:48 -0300

    Update GameManager.cs
    Opening of the headers for the player's and enemies' game objects. Animation of the player sprite with DG.Tweening, and implementation of the player's spawn logic.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,42 @@
-using UnityEngine;
+using DG.Tweening;
 using EDGEE.Core.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+    private GameObject _currentPlayer;
+
+    [Header("Animation")]
+    public float duration = 0.5f;
+    public float delay = 0.02f;
+    public Ease ease = Ease.OutBack;
+
+    private void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position=startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetDelay(0).SetEase(ease).From();
+    }
 
 }
