6db2a89c0eee783e993758676c3d03f00ca29c8d
Wilber <wilber.sa@hotmail.com>
2026-01-26 19:15:06 -0300

    Adjustments to the pause screen and other scripts
    Scriptable objects were created to adjust the data of coins collected and enemies defeated on the end-game screen.

--- Assets/Scripts/Utils/UI-Interface/SOInt.cs
+++ Assets/Scripts/Utils/UI-Interface/SOInt.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu (fileName = "NewSOInt", menuName = "So/Int")]
 
 public class SOInt : ScriptableObject
 {
     public int valueInt;
+
+    public void Add(int amount)
+    {
+        valueInt += amount;
+    }
 
 }
