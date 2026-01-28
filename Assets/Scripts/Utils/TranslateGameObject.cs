5ecb2554aeb499a0ff9dd917b6585af21f80a33c
Wilber <wilber.sa@hotmail.com>
2026-01-28 20:37:20 -0300

    Inclusion of a script for planet and smoke movement

---
+++ Assets/Scripts/Utils/TranslateGameObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TranslateGameObject : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
